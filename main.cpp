08065e1a292555f5621c61e970e856621faaf6d9
Gabriel Wallace <wallace004@live.missouristate.edu>
2018-12-04 18:56:34 -0600

    Done

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <queue>
 #include <tuple>
+#include <fstream>
+#include <string>
 
 typedef std::tuple<int, int, char> event;
 
@@ -14,24 +16,41 @@ using std::queue;
 using std::vector;
 using std::greater;
 using std::get;
+using std::make_tuple;
 using std::cout;
 using std::endl;
+using std::string;
 
-void simulate();
+void simulate(const string &);
 
 int main() {
-    simulate();
+    simulate("../bankData.txt");
     return EXIT_SUCCESS;
 }
 
-void simulate() {
+void simulate(const string & fileName) {
     priority_queue<event, vector<event>, greater<event>> eventQueue;
     queue<event> bankLine;
 
     bool tellerAvailable{true};
     int totalWaitTime{0};
+    int customersProcessed{0};
 
-    //while( DATA FILE IS NOT EMPTY)
+    //Creating the initial eventQueue
+    string line;
+    std::ifstream myFile (fileName);
+    while(getline(myFile, line)) {
+        int comma = line.find_first_of(",");
+        string time = line.substr(0, comma);
+        string leng = line.substr(comma + 1);
+        event newArrivalEvent = make_tuple(std::stoi(time), std::stoi(leng), 'A');
+        eventQueue.push(newArrivalEvent);
+        customersProcessed++;
+    }
+    myFile.close();
+
+    //Simulation Begins
+    cout << "Simulation Begins" << endl;
 
     while (!eventQueue.empty()) {
         event newEvent = eventQueue.top();
@@ -41,9 +60,10 @@ void simulate() {
 
         //if newEvent is an Arrival
         if (get<2>(newEvent) == 'A') {
+            cout << "Processing an arrival event at time: " << currentTime << endl;
             if (bankLine.empty() && tellerAvailable) {
-                int departureTime = currentTime + get<0>(newEvent);
-                event newDeptEvent = std::make_tuple(departureTime, 0, 'D');
+                int departureTime = currentTime + get<1>(newEvent);
+                event newDeptEvent = make_tuple(departureTime, 0, 'D');
                 eventQueue.push(newDeptEvent);
                 tellerAvailable = false;
             } else {
@@ -53,22 +73,25 @@ void simulate() {
 
         //if newEvent is a Departure
         if (get<2>(newEvent) == 'D') {
+            cout << "Processing a departure event at time: " << currentTime << endl;
             if (!bankLine.empty()) {
                 event customer = bankLine.front();
                 bankLine.pop();
-                int departureTime = currentTime + get<1>(newEvent);
-                event newDeptEvent = std::make_tuple(departureTime, 0, 'D');
+                int departureTime = currentTime + get<1>(customer);
+                event newDeptEvent = make_tuple(departureTime, 0, 'D');
                 eventQueue.push(newDeptEvent);
-                totalWaitTime += currentTime - get<1>(bankLine.front());
+                totalWaitTime += currentTime - get<0>(customer);
             } else {
                 tellerAvailable = true;
             }
         }
     }
-        cout << "Final Statistics:" << endl;
-        cout << "\t Total number of people processed: " << endl;
-        cout << "\t Average amount of time spent waiting: " << totalWaitTime << endl;
 
+    //Final Stats
+    double average = static_cast<double>(totalWaitTime) / customersProcessed;
+    cout << "Simulation Ends\n" << endl;
+    cout << "Final Statistics:" << endl;
+    cout << "\t Total number of people processed: " <<  customersProcessed << endl;
+    cout << "\t Average amount of time spent waiting: " << average << endl;
 }
-
 
